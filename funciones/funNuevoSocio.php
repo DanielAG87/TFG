a9f7f3b0f245329935b8c99fde336c93ed867df1
Daniel Agustin Arroyo <bicverde323@gmail.com>
2023-05-10 19:57:48 +0200

    Funcionalidad total en añadir ususario (terminado)

--- funciones/funNuevoSocio.php
+++ funciones/funNuevoSocio.php
@@ -3,12 +3,12 @@ include_once "../conectarBBDD.php";
 
 function addNuevoSocio(){
 
-    $nombre = $_REQUEST['nombre'];
-    $ape1 = $_REQUEST['ape1']; 
-    $ape2 = $_REQUEST['ape2']; 
+    $nombre = ucwords($_REQUEST['nombre']);
+    $ape1 = ucwords($_REQUEST['ape1']); 
+    $ape2 = ucwords($_REQUEST['ape2']); 
     $correo = $_REQUEST['correo']; 
     $tel = $_REQUEST['tel']; 
-    $loca = $_REQUEST['loca']; 
+    $loca = ucwords($_REQUEST['loca']); 
     $fechaNac = $_REQUEST['fechaNac']; 
     $contra = $_REQUEST['contra']; 
     $permiso = $_REQUEST['permiso'];
@@ -69,64 +69,86 @@ function addNuevoSocio(){
     else {
         $contador ++;
     }
+
+
     if ($contador == 9) {
+      
+        $contador2 = 0;
         
-        // hacer patrones y comprobarlos
-        $comprobar = false;
-        $con = conectarBD();
-
-        $hash = sha1($contra);
-       
-
-        // $expNombre = preg_match('/^[A-Za-z\s]{1,20}$/', $nombre);
-        if (preg_match('/^[A-Za-z\s]{1,20}$/', $nombre)) {
-            echo "exito";
+        // control del nombre y apellidos
+        if (preg_match('/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s-]{1,20}$/u', $nombre) && preg_match('/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s-]{1,20}$/u', $ape1) && preg_match('/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s-]{1,20}$/u', $ape2)) {
+            $contador2++;
         }
         else {
-            echo "Fallo";
+            echo "Falla nombre, o apellido1 o apellido2";
+        }
+       
+        // controla el correo
+        if (filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+            $contador2++;
+        }
+        else {
+            echo "Falla correo";
         }
 
+        // controla el telefono
+        if (preg_match('/^(?:\+34|0034)?[6789]\d{8}$/', $tel) ) {
+            $contador2++;
+        }
+        else {
+            echo "Falla telefono";
+        }
+        // controla la localidad
+        if (preg_match('/^[a-zA-Z\s-]+$/', $loca) ) {
+            $contador2++;
+        }
+        else {
+            echo "Falla localidad";
+        }
+      
 
+        // si todo está correcto hacemos el insert.
+        if ($contador2 == 4) {
 
+            $con = conectarBD();
 
+            if ($permiso == "Si") {
+                $permiso = 1;
+            }
+            else {
+                $permiso = 0;
+            }
+           
+            $hash = sha1($contra);
+            $nuevoFulano = "INSERT INTO socios (nombre, apellido1, apellido2, correo, telefono, localidad, fecha_nacimiento, contrasenia, permiso) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $stmt = mysqli_stmt_init($con);
 
+            if (mysqli_stmt_prepare($stmt, $nuevoFulano)){
 
+                if(mysqli_stmt_bind_param($stmt, "ssssssssi", $nombre, $ape1, $ape2, $correo, $tel, $loca, $fechaNac, $hash, $permiso)){
 
+                    if(mysqli_stmt_execute($stmt)){
+                        echo mysqli_affected_rows($con). " " . "Socio añadido"; 
 
-
-
-
-
-
-
-
-
-        
-        // $nuevoFulano = "INSERT INTO socios (nombre, apellido1, apellido2, correo, telefono, localidad, fecha_nacimiento, contrasenia, permiso) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        // $stmt = mysqli_stmt_init($con);
-
-        // if (mysqli_stmt_prepare($stmt, $nuevoFulano)){
-
-        //         if(mysqli_stmt_bind_param($stmt, "ssssssssi", $nombre, $ape1, $ape2, $correo, $tel, $loca, $fechaNac, $hash, $permiso)){
-
-        //             if(mysqli_stmt_execute($stmt)){
-        //                 echo mysqli_affected_rows($con). " " . "Socio añadido"; ?>
-        <!-- //                 <form method="post" action="socios.php">
-        //                     <input type="submit" name="vovler1" value="Volver"/>
-        //                 </form> -->
-        //             <?php
-        //             }
-        //             else{
-        //                 echo "Error de introducción";
-        //             }
-        //         mysqli_stmt_close($stmt);
-        //         }
-        // }
-        // else{
-        //     echo "No se ha podido completar la accion, Prueba más tarde";
-        // }
+                        echo '<form method="post" action="socios.php">
+                             <input type="submit" name="vovler1" value="Volver a Socios"/>
+                         </form>' ;
+                      
+                    }
+                    else{
+                        echo "Error de introducción";
+                    }
+                mysqli_stmt_close($stmt);
+                }
+            }
+            else{
+            echo "No se ha podido completar la accion, Prueba más tarde";
+            }
+        }
+       
     }
-    
 }
 
-addNuevoSocio(); ?>
+addNuevoSocio(); 
+
+?>
--- nuevoSocio.php
+++ nuevoSocio.php
@@ -5,36 +5,45 @@ include "headerV2.php";
 <div class="container-fluid p-5">
     <div class="row">
         <div class="col-md-4 mb-3">
+            <label for="nombre">Nombre</label>
             <input type="text" id="nombre" class="form-control" placeholder="Nombre">
         </div>
         <div class="col-md-4 mb-3">
+        <label for="nombre">Primer Apellido</label>
             <input type="text" id="ape1" class="form-control" placeholder="Primer Apellido">
         </div>
         <div class="col-md-4 mb-3">
+        <label for="nombre">Segundo Apellido</label>
             <input type="text" id="ape2" class="form-control" placeholder="Segundo Apellido">
         </div>
     </div>
     <div class="row">
         <div class="col-md-6 mb-3">
-            <input type="mail" id="correo" class="form-control" placeholder="Correo">
+        <label for="nombre">Correo</label>
+            <input type="mail" id="correo" class="form-control" placeholder="asociacionRunaBlanca@gmail.com">
         </div>
         <div class="col-md-6 mb-3">
+        <label for="nombre">Teléfono</label>
             <input type="text" id="tel" class="form-control" placeholder="Teléfono">
         </div>
     </div>
     <div class="row">
         <div class="col-md-4 mb-3">
+        <label for="nombre">Localidad</label>
             <input type="text" id="loca" class="form-control" placeholder="Localidad">
         </div>
         <div class="col-md-4 mb-3">
+        <label for="nombre">Fecha de nacimiento</label>
             <input type="date" id="fechaNac" class="form-control" placeholder="Fecha de nacimiento">
         </div>
         <div class="col-md-4 mb-3">
+        <label for="nombre">Contraseña</label>
             <input type="text" id="contra" class="form-control" placeholder="Contraseña">
         </div>
     </div>
     <div class="row">
         <div class="col-md-12 mb-3">
+        <label for="nombre">Permiso</label>
             <select id="permiso" class="form-control">
                 <option disabled selected>Selecciona una opción</option>
                 <option>Si</option>
@@ -74,7 +83,7 @@ include "headerV2.php";
                 document.getElementById('result').innerHTML = this.responseText;
             }
         };
-        xhttp.open("GET", "funciones/funNuevoSocio.php?nombre=" + nombre + "&ape1="+ ape1 + "&ape2="+ ape2 + "&correo="+ correo + "&tel="+ tel
+        xhttp.open("POST", "funciones/funNuevoSocio.php?nombre=" + nombre + "&ape1="+ ape1 + "&ape2="+ ape2 + "&correo="+ correo + "&tel="+ tel
         + "&loca="+ loca + "&fechaNac="+ fechaNac + "&contra="+ contra + "&permiso="+ permiso, true); 
         xhttp.send();
     }
@@ -89,7 +98,9 @@ $(document).ready(function() {
   // codigo para limpiar los campos de los imput
     $(document).ready(function() {
         $("#formSocio").click(function() {
-            $("#nombre, #ape1, #ape2, #correo, #tel, #loca, #fechaNac, #contra, #permiso").val("");
+            $("#nombre, #ape1, #ape2, #correo, #tel, #loca, #fechaNac, #contra").val("");
+            $("#permiso").prop("selectedIndex", 0);
+  
         });
     });
 });
